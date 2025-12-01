183f7e94fec3dd71eb7bfd992c3ac2f7ab134327
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-12-01 23:35:00 +0100

    Part 2

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "aoc/solutions"
 
 func main() {
-	solutions.Day_01_part_01()
+	// solutions.Day_01_part_01()
+	solutions.Day_01_part_02()
 }
--- solutions/day_01.go
+++ solutions/day_01.go
@@ -3,11 +3,43 @@ package solutions
 import (
 	"aoc/utils"
 	"fmt"
+	"math"
 	"strconv"
 )
 
 func Day_01_part_01() {
-	input := utils.Read_lines(1, true)
+	input := utils.Read_lines(1, false)
+
+	pointer := 50
+	pass := 0
+
+	for _, line := range input {
+		rotation, times := split_rotation(line)
+
+		var result int
+		if rotation == "R" {
+			result = pointer + times
+		} else {
+			result = pointer - times
+			if result < 0 {
+				result = result + 100
+			}
+		}
+
+		pointer = result % 100
+
+		fmt.Printf("%s    %d\n", line, pointer)
+
+		if pointer == 0 {
+			pass++
+		}
+	}
+
+	fmt.Println(pass)
+}
+
+func Day_01_part_02() {
+	input := utils.Read_lines(1, false)
 
 	pointer := 50
 	pass := 0
@@ -16,20 +48,35 @@ func Day_01_part_01() {
 		rotation, times := split_rotation(line)
 
 		if times >= 100 {
+			pass = pass + int(math.Floor(float64(times)/float64(100)))
 			times = times % 100
 		}
 
+		var result int
 		if rotation == "R" {
-			pointer = (pointer + times) % 100
+			result = pointer + times
+			if result > 100 {
+				pass++
+			}
 		} else {
-			x := pointer - times + 100
-			pointer = max(x, -x) % 100
+			result = pointer - times
+			if result < 0 {
+				result = max(result, -result)
+				result = 100 - result
+
+				if pointer != 0 {
+					pass++
+				}
+			}
 
 		}
+
+		pointer = result % 100
 
 		if pointer == 0 {
 			pass++
 		}
+
 	}
 
 	fmt.Println(pass)
