481d697e0a0376b0570504475e0f74c078908ec0
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-12-02 06:21:19 +0100

    Day 02 start

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "aoc/solutions"
 
 func main() {
 	// solutions.Day_01_part_01()
-	solutions.Day_01_part_02()
+	// solutions.Day_01_part_02()
+	solutions.Day_02_part_01()
 }
--- solutions/day_02.go
+++ solutions/day_02.go
@@ -1,1 +1,21 @@
 package solutions
+
+import (
+	"aoc/utils"
+	"fmt"
+	"strings"
+)
+
+func Day_02_part_01() {
+	input := utils.Read_lines(1, false)
+	input = strings.Split(input[0], ",")
+
+	fmt.Println(input)
+}
+
+func Day_02_part_02() {
+	input := utils.Read_lines(1, false)
+	input = strings.Split(input[0], ",")
+
+	fmt.Println(input)
+}
